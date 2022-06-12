58aced1e24c56a0d01ac284f2ef2d456690f29a8
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-12 19:44:18 -0300

    listas

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const precosLivros = [25, 15, 30, 50, 45, 20];
 let maisBarato = 0;
 
 for (let atual = 0; atual < precosLivros.length; atual++) {
-    if (precosLivros[atual] < precosLivros[maisBarato]) {
+    if (livros[atual].preco < precosLivros[maisBarato]) {
         maisBarato = atual
     }
 }
 
-console.log('O livro mais barati custa ${precosLivros[maisBarato]}')
+console.log('O livro mais barati custa ${livros[maisBarato].preco e o titulo é [maisBarato].titulo}')
