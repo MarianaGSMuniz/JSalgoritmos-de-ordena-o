daacc12ba41b0f702b01624964eec7cdb8f584a1
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-13 19:00:08 -0300

    sort

--- sort.js
+++ sort.js
@@ -2,5 +2,10 @@ const livros = require('./lista-livros.js');
 const menorValor = require('./menorvalor');
 
 for (let atual = 0; atual < livros.length; atual++) {
+    let = menorValor(livros, atual)
+
+    let livroAtual = livros[atual];
+
+    let livroMenorPreco = livros[menor];
 
 }
