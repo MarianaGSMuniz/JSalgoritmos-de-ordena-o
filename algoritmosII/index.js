5e3de549703e827c75102aefc1954d23f1ba4687
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-15 21:49:19 -0300

    arrays

---
+++ algoritmosII/index.js
@@ -0,0 +1,8 @@
+const { edGalho, edFolha } = require('./array.js');
+
+function juntaLista(lista1, lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+
+}
