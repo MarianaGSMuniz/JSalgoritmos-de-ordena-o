4a596163b4e35ea4e6805e8cfd1d76f14c865280
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-22 21:15:47 -0300

    Ordenação pelo pivô

--- aula3/encontraMenores.js
+++ aula3/encontraMenores.js
@@ -25,4 +25,22 @@ function trocaLugar(array, de, para) {
     return menores
 }
 
+function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    encontrarMenores(pivo, array);
+    let valoresMenores = 0;
+
+    for (let analisando = 0; analisando < array.length; analisando++) {
+        let atual = array[analisando];
+        if (atual.preco <= pivo.preco && atual !== pivo) {
+            trocaLugar(array, analisando, valoresMenores)
+            valoresMenores++
+        }
+    }
+
+
+    return array
+}
+
+
 console.log(encontraMenores(listaLivros[2], listaLivros));
