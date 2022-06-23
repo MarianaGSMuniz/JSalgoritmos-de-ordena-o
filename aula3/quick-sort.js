3715212931239c9520628ee217c0fe2f2509845b
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-22 22:40:51 -0300

    Ordenação pelo pivô

--- aula3/quick-sort.js
+++ aula3/quick-sort.js
@@ -1,14 +1,45 @@
 const listaLivros = require('./array')
+const trocaLugar = require('./encontraMenores')
 
 function quickSort(array, esquerda, direita) {
     if (array.length > 1) {
-        let varTemp = particiona(array, esquerda, direita);
+        let indiceAtual = particiona(array, esquerda, direita);
+        if (esquerda < indiceAtual - 1) {
+
+            quickSort(array, esquerda, indiceAtual - 1)
+        }
+        if (indiceAtual < direita) {
+            quickSort(array, indiceAtual, direita)
+        }
     }
     return array;
 }
 
 function particiona(array, esquerda, direita) {
     let pivo = array[Math.floor(esquerda + direta / 2)]
+} {
+    let atualEsquerda = esquerda;
+    let atualDireita = direita;
+
+    while (atualEsquerda <= atualDireita) {
+        while (array[atualEsquerda].preco < pivo.preco) {
+            atualEsquerda++
+        }
+        while (array[atualDireita].preco > pivo.preco) {
+            atualDireita--
+
+        }
+
+        if (atualEsquerda <= atualDireita)
+            trocaLugar(array, atualEsquerda, atualDireita);
+        atualEsquerda++;
+        atualEsquerda--;
+    }
 }
+return atualEsquerda;
+
+
+
+
 
 console.log(quickSort(listaLivros, 0, listaLivros.length - 1))
